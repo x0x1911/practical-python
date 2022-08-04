632b3b655396d713840ae0a9c0ece53d675ad6df
x0x1911 <asylum717@protonmail.com>
2022-08-04 11:18:01 +0700

    Sync

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -5,11 +5,21 @@
 import csv
 
 
-def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','):
-    '''
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
+    """
     Parse a CSV file into a list of records
-    '''
+
+    filename: a string 'folder/file.csv'.
+    select: a list of strings ['column name1, column name2, ...]
+        optionally allows user-specified columns to be picked.
+    types: optionally allows type-conversions to be applied to the returned data
+        types=[str, int, float].
+    has_headers: optionally allows to parse csv files without header has_headers=False.
+    delimiter: allows to specify delimiter default is: delimiter=','.
+    """
     with open(filename) as f:
+        if select and not has_headers:
+            raise RuntimeError("select argument requires column headers")
         rows = csv.reader(f, delimiter=delimiter)
 
         # Read the file headers
@@ -19,16 +29,24 @@ def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=','
             indices = [headers.index(colname) for colname in select]
             headers = select
         records = []
-        for row in rows:
+        for rownumber, row in enumerate(rows, start=1):
             if not row:
                 continue
             if select:
                 row = [row[index] for index in indices]
             if types:
-                row = [func(val) for func, val in zip(types, row)]
+                try:
+                    row = [func(val) for func, val in zip(types, row)]
+                except Exception as e:
+                    if silence_errors:
+                        continue
+                    else:
+                        print(f'Row {rownumber}:  Couldn\'t convert {row}')
+                        print(f'Row {rownumber}: Reason {e}')
             if has_headers:
                 record = dict(zip(headers, row))
             else:
                 record = tuple(row)
             records.append(record)
     return records
+portfolio = parse_csv('Data/missing.csv', types=[str, int, float])
